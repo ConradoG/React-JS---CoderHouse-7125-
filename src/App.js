bff6aad681ed189f61292c40df5af9b0a8e83e68
conradogassa@gmail.com <you@example.com>
2020-10-12 13:30:41 -0300

    primer commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Img from './components/img'
+import Title from './components/Title'
+import ReactLink from './components/ReactLink'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Img />
+        <Title />
+        <ReactLink />
+      </header>
+    </div>
+  );
+}
+
+export default App;
