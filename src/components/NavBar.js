5dbedb41675bb845e51b8fe020748e5668ffe367
conradogassa@gmail.com <you@example.com>
2020-10-18 11:00:20 -0300

    agregué NavBar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -31,16 +31,6 @@ return <nav className="navbar navbar-expand-lg navbar-light bg-light">
   </div>
 </div>
 
-<div className="dropdown">
-  <button className="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenu2" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-    Dropdown
-  </button>
-  <div className="dropdown-menu" aria-labelledby="dropdownMenu2">
-    <button className="dropdown-item" type="button">Action</button>
-    <button className="dropdown-item" type="button">Another action</button>
-    <button className="dropdown-item" type="button">Something else here</button>
-  </div>
-</div>
  </ul>
 
     <form className="form-inline my-2 my-lg-0">
